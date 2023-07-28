233655090c573984c90ffdedcae50768bc72b94d
Alexandr Liashenko <134228068+AlexLiashenko19@users.noreply.github.com>
2023-07-28 09:46:17 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+refs = {
+    bodyEl: document.querySelector("body"),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+};
+
+refs.btnStop.setAttribute('disabled', true);
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+refs.btnStart.addEventListener("click", onClickStart);
+refs.btnStop.addEventListener("click", onClickStop);
+
+
+function onClickStart(e) {
+    bodyChangeColor()
+    timerId = setInterval(() => {
+        bodyChangeColor();
+      }, 1000);  
+      e.target.setAttribute('disabled', true);
+      refs.btnStop.disabled = false;
+}
+
+function bodyChangeColor(e) {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function onClickStop(e) {
+    clearInterval(timerId);
+    e.target.setAttribute('disabled', true);
+    refs.btnStart.disabled = false; 
+}
 
